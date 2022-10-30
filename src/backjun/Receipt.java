97ce6a47caa726d071bb2c4e0aa9b6f3bf9e54e8
alstlr0312 <alstlr0312@naver.com>
2022-10-30 15:27:59 +0900

    25304

---
+++ src/backjun/Receipt.java
@@ -0,0 +1,20 @@
+package backjun;
+import java.util.Scanner;
+
+public class Receipt {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int X = sc.nextInt();
+        int N = sc.nextInt();
+
+        for(int i=0; i<N; i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            X = X - (a*b);
+        }
+        if(X == 0) System.out.println("Yes");
+        else System.out.println("No");
+    }
+}
